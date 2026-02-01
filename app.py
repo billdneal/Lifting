9a6c45030a16bfd0ef31c7fc4f3128319761a05c
billdneal <bill.d.neal@gmail.com>
2026-02-01 16:14:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 from datetime import date
-
 # ==========================================
 # 1. CONFIG & CACHING
 # ==========================================
